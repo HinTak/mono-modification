a66e65596d2b8284ecc126d012d04196cd214db5
Hin-Tak Leung <htl10@users.sourceforge.net>
2020-05-26 22:33:49 +0100

    Detects fat binaries and refuses to modify them

--- mono-codesign-fix.py
+++ mono-codesign-fix.py
@@ -23,6 +23,12 @@ exe_data = MachO(input_binary)
 
 signed_binary = False
 
+multi_arch = False
+
+if (len(exe_data.headers) > 1):
+    print("Fat Binary:", len(exe_data.headers))
+    multi_arch = True
+
 # Fat binary could contain multiple architectures.
 for h in exe_data.headers:
     # Access Mach-O load commands
@@ -32,7 +38,7 @@ for h in exe_data.headers:
         if (c[0].get_cmd_name() == 'LC_SYMTAB'):
             print("entry(LC_SYMTAB):\n\t", c[0].get_cmd_name(), c[1], c[2])
             print(file_size, c[1].stroff + c[1].strsize)
-            if (file_size != c[1].stroff + c[1].strsize):
+            if ((file_size != c[1].stroff + c[1].strsize) and (not multi_arch)):
                 c[1].strsize = file_size - c[1].stroff
         if (c[0].get_cmd_name() == 'LC_CODE_SIGNATURE'):
             print("entry(LC_CODE_SIGNATURE):\n\t", c[0].get_cmd_name(), c[1], c[2])
@@ -41,12 +47,12 @@ for h in exe_data.headers:
     linkedit = h.commands[3]
     print("4th(LC_SEGMENT_64/__LINKEDIT):\n\t", linkedit[0].get_cmd_name(), linkedit[1], linkedit[2])
     print(file_size, linkedit[1].fileoff + linkedit[1].filesize, ((linkedit[1].vmsize - linkedit[1].filesize) == 0))
-    if (file_size != linkedit[1].fileoff + linkedit[1].filesize):
+    if (file_size != linkedit[1].fileoff + linkedit[1].filesize and (not multi_arch)):
         linkedit[1].filesize = file_size - linkedit[1].fileoff
         linkedit[1].vmsize = linkedit[1].filesize
 
-if (signed_binary):
-    print("The binary was signed. Not modifying.")
+if (signed_binary or multi_arch):
+    print("The binary was signed or multi_arch. Not modifying.")
 else:
     # Change some header parameters.
     # Write changes back.

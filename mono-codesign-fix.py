a360a5b1be109c816847e4d3874f6ad2bf53ac8b
Hin-Tak Leung <htl10@users.sourceforge.net>
2021-06-28 20:32:12 +0100

    Trying to detect mkbundle run with "default   - Current System Mono" which is already signed

--- mono-codesign-fix.py
+++ mono-codesign-fix.py
@@ -23,6 +23,12 @@ exe_data = MachO(input_binary)
 
 signed_binary = False
 
+# The end of signature is expected to be at the end of file,
+# while beginning of bundle is after the main mono binary
+# and definitely not the end.
+end_of_signature = 0
+beginning_of_bundle = file_size
+
 multi_arch = False
 
 # Development option to quickly disable modifying
@@ -46,7 +52,7 @@ for h in exe_data.headers:
                 c[1].strsize = file_size - c[1].stroff
         if (c[0].get_cmd_name() == 'LC_CODE_SIGNATURE'):
             print("entry(LC_CODE_SIGNATURE):\n\t", c[0].get_cmd_name(), c[1], c[2])
-            signed_binary = True
+            end_of_signature = c[1].dataoff + c[1].datasize
     # The 4th (last) 'LC_SEGMENT_64' is the __LINKEDIT segment.
     linkedit = [c for c in h.commands if \
         hasattr(c[1], 'segname') and \
@@ -56,8 +62,20 @@ for h in exe_data.headers:
     if (linkedit[0].get_cmd_name() == 'LC_SEGMENT_64'):
         print(file_size, linkedit[1].fileoff + linkedit[1].filesize, ((linkedit[1].vmsize - linkedit[1].filesize) == 0))
         if (file_size != linkedit[1].fileoff + linkedit[1].filesize and (not multi_arch)):
+            beginning_of_bundle = linkedit[1].fileoff + linkedit[1].filesize
+            print("Beginning of bundle", beginning_of_bundle)
             linkedit[1].filesize = file_size - linkedit[1].fileoff
             linkedit[1].vmsize = linkedit[1].filesize
+
+if (end_of_signature != 0):
+    if (end_of_signature != file_size):
+        print("End of Signature not at end of file:", end_of_signature)
+    if (beginning_of_bundle != file_size):
+        if (beginning_of_bundle == end_of_signature):
+            print("mkbundle with Native Mac OS X/default runtime detected. This may not work.")
+
+if (end_of_signature == file_size):
+    signed_binary = True
 
 if (signed_binary or multi_arch or dev_not_modifying):
     print("The binary was signed or multi_arch. Not modifying.")
